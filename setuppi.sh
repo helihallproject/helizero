b168c1f49e5ebb530b956bd9f7c5ed1cb8ce045a
helihallproject <48986627+helihallproject@users.noreply.github.com>
2024-05-10 21:41:38 +1000

    Create setuppi.sh

---
+++ setuppi.sh
@@ -0,0 +1,7 @@
+# Git clone then run locally to set up a new Pi Zero
+
+sudo apt update
+sudo apt upgrade
+sudo rm /usr/lib/python3.11/EXTERNALLY-MANAGED
+sudo apt install python3-pip
+sudo pip3 install mysql-connector-python
