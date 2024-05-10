71e937e1ce8339629c31592d91ecbcc84229b404
helihallproject <48986627+helihallproject@users.noreply.github.com>
2024-05-10 21:46:14 +1000

    Update setuppi.sh

--- setuppi.sh
+++ setuppi.sh
@@ -1,7 +1,20 @@
 # Git clone then run locally to set up a new Pi Zero
 
+
+# Standard
 sudo apt update
 sudo apt upgrade
+
+# Remove the error about externally managed system
 sudo rm /usr/lib/python3.11/EXTERNALLY-MANAGED
+
+# Install Pip 3, if missing
 sudo apt install python3-pip
+
+# Install mysql connector
 sudo pip3 install mysql-connector-python
+
+# Git clone the startup scripts
+# Create Project directory
+# Create service to start scripts on boot
+
